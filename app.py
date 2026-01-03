4484dc2581ba0dffd74869fabefe2c47203e612e
SoftwareFail <softwarefail@yahoo.com>
2026-01-03 02:54:47 -0800

    update app.py port to 8080

--- app.py
+++ app.py
@@ -233,4 +233,4 @@ def media_file(filename):
  # ---------------------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=8080,debug=True)
